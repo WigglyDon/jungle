ff1975fb32e415afe07750585823d2a280082b69
Donnie Walsh <donfather1000@gmail.com>
2021-11-13 03:27:40 +0000

    add stats to admin dash

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['BA_USER'], password: ENV['BA_PASS']
  
   def show
+    @product_count = Product.all.count
+    @category_count = Category.all.count
   end
 end
